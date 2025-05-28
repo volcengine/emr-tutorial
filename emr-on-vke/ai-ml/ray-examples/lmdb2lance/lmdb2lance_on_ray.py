d2187587838134f8e2eb1f3e0ac97d0ccc32dafb
jay <jukejian@bytedance.com>
2025-05-28 17:32:31 +0800

    [ray][lance]fix lmdb to lance demo (#9)

--- emr-on-vke/ai-ml/ray-examples/lmdb2lance/lmdb2lance_on_ray.py
+++ emr-on-vke/ai-ml/ray-examples/lmdb2lance/lmdb2lance_on_ray.py
@@ -4,7 +4,7 @@ import pyarrow as pa
 import ray
 
 
-LMDB_PATHS = ['./image_lmdb/']
+LMDB_PATHS = ['./image_data.lmdb/']
 LANCE_PATH = "./lmdb_to_lance.lance"
 STORAGE_OPTIONS = {}
 SCHEMA = pa.schema([
@@ -12,8 +12,7 @@ SCHEMA = pa.schema([
 ])
 
 
-def lmdb_to_arrow_table(row):
-  file_path = row["item"]
+def lmdb_to_arrow_table(file_path: str):
   frame_keys = []
   data = {
     "camera_left": [],
@@ -26,11 +25,19 @@ def lmdb_to_arrow_table(row):
   for frame_key in frame_keys:
     data['camera_left'].append(label_ctx.get(frame_key))
 
-  return data
+  table = pa.Table.from_pydict(data, schema=SCHEMA)
+  return table
+
+
+def transform_lmdb(row: dict[str, any]):
+  import json
+  lmdb_file = row["item"]
+  table = lmdb_to_arrow_table(lmdb_file)
+  fragment = lance.fragment.LanceFragment.create(LANCE_PATH, table, storage_options=STORAGE_OPTIONS)
+  return {"fragment": json.dumps(fragment.to_json())}
 
 
 def initialize_lance_dataset():
-  # 创建空数据集
   empty_table = pa.Table.from_arrays(
       [pa.array([], pa.large_binary())],
       names=["camera_left"]
@@ -38,7 +45,13 @@ def initialize_lance_dataset():
   lance.write_dataset(empty_table, LANCE_PATH, schema=SCHEMA)
 
 if __name__ == '__main__':
-  # initialize_lance_dataset()
-  ds = ray.data.from_items(LMDB_PATHS)
-  ds = ds.map(lmdb_to_arrow_table)
-  ds = ds.write_lance(LANCE_PATH, schema = SCHEMA)
+  initialize_lance_dataset()
+  ds = ray.data.from_items(LMDB_PATHS, override_num_blocks=100)
+  fragments = ds.map(transform_lmdb).take_all()
+  operation = lance.LanceOperation.Append([lance.fragment.FragmentMetadata.from_json(fragment_json['fragment']) for fragment_json in fragments])
+  dataset = lance.dataset(LANCE_PATH, storage_options=STORAGE_OPTIONS)
+  dataset = lance.LanceDataset.commit(LANCE_PATH, operation, read_version=dataset.latest_version,
+                                      storage_options=STORAGE_OPTIONS)
+
+  lance_ds = lance.dataset(LANCE_PATH)
+  print(f"schema:: {lance_ds.schema},  total rows = {lance_ds.count_rows()}")
